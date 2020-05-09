1ac52e2fc8a7ac840c34696689d7c88d110bfd89
Viktor <v.gagaleski@yahoo.com>
2020-05-09 19:33:14 +0200

    Major refactoring to Vanilla JavaScript

--- popup.js
+++ popup.js
@@ -11,58 +11,99 @@
    at TestExceptionGenerator.Extensions.GetExceptionString(Action action) in c:\Code\personal\DotNetExceptionMessageFormatter\TestExceptionGenerator\Spike.cs:line 56
  *
  */
-// Search the bookmarks when entering the search keyword.
-$(function() {
 
-  modalSetup()
+document.addEventListener("load", () => { 
+  console.log("load...")
+});
 
-  $('#input').change(function() {
-     $('#output').val(formatStackTrace($('#input').val()));
+
+document.addEventListener("DOMContentLoaded", () => { 
+  console.log("DOMContentLoaded...")
+  initializeTheme();
+  initializeSaveToClipboard();
+
+  let inputBox = document.getElementById('input');
+  let outputBox = document.getElementById('output');
+  let clearBtn = document.getElementById('clearBtn');
+  let settingsBtn = document.getElementById('settingsBtn');
+
+  inputBox.addEventListener('paste', (event) => {
+
+    let paste = (event.clipboardData || window.clipboardData).getData('text');
+    inputBox.value = paste;
+
+    outputBox.value = formatStackTrace(paste);
+    if (isSaveToClipboardToggled()) {
+      copyToClipboard();
+    }
+
   });
 
-  $('#input').keypress(function (e) {
-    if (e.which == 13) {
-      $('#output').val(formatStackTrace($('#input').val()));
-      if(isSaveToClipboardToggled()) {
+  inputBox.addEventListener('keydown', (event) => {
+   
+    if (event.key === 'Enter') {
+      event.preventDefault();
+
+      outputBox.value = formatStackTrace(inputBox.value);
+      if (isSaveToClipboardToggled()) {
         copyToClipboard();
       }
-      return false;    //<---- Add this line
     }
   });
 
-  $("#input").on("paste", function () {
-    setTimeout(function() {
-      $('#output').val(formatStackTrace($('#input').val()));
-      if(isSaveToClipboardToggled()) {
-        copyToClipboard();
-      }
-      
-    });
-  });
+  clearBtn.addEventListener('click', () => {
+    inputBox.value = '';
+    outputBox.value = '';
+  })
 
-  $("#clearBtn").click(function() {
-    $('#input').val('')
-    $('#output').val('')
-  });
-
-  $('#settingsBtn').click(function() {
-    openModal()
-    // initializeSelectionComponent()
-  });
+  settingsBtn.addEventListener('click', () => {
+    openModal();
+  })
 
 });
 
+openModal = () => {
+  var modalContent = document.getElementById('modal-content');
+  var modalBackground = document.getElementById('modal-bg');
+
+  initializeModal();
+
+  modalContent.classList.add('open');
+  modalBackground.classList.add('open');
+}
+
 // Modal is initialized as many times as we click settings btn
-function modalSetup() {
+initializeModal = () => {
   
-  var saveButton = document.querySelector("#saveBtn");
-  var closeButton = document.querySelector("#closeBtn");
-  var closeXButton = document.querySelector('#closeXBtn');
-  var toggle = document.querySelector("#toggle-checkbox");
+  var saveButton = document.getElementById('saveBtn');
+  var closeButton = document.getElementById('closeBtn');
+  var closeXButton = document.getElementById('closeXBtn');
+  var copyToClipboardToggle = document.getElementById('toggleCopyToClipboard');
+  var darkThemeToggle = document.getElementById('toggleDarkTheme');
+
+  var modalContent = document.getElementById('modal-content');
+  var modalBackground = document.getElementById('modal-bg');
+  
+  initializeSaveToClipboardToggle();
+  initializeDarkThemeToggle();
+
+  darkThemeToggle.addEventListener("change", function() {
+    if (darkThemeToggle.checked) {
+      chrome.storage.local.set({'darkThemeToggle': true}, function() {
+        console.log('Value is set to ' + true);
+      });
+      setTheme(true);
+    } else {
+      chrome.storage.local.set({'darkThemeToggle': false}, function() {
+        console.log('Value is set to ' + false);
+      });
+      setTheme(false);
+    }
+  });
 
   saveButton.addEventListener("click", function() {
 
-    if (toggle.checked) {
+    if (copyToClipboardToggle.checked) {
       chrome.storage.local.set({'copyToClipboardToggle': true}, function() {
         console.log('Value is set to ' + true);
       });
@@ -72,49 +113,59 @@ function modalSetup() {
       });
     }
 
-    closeModal();
+    if (darkThemeToggle.checked) {
+      chrome.storage.local.set({'darkThemeToggle': true}, function() {
+        console.log('Value is set to ' + true);
+      });
+      setTheme(true);
+    } else {
+      chrome.storage.local.set({'darkThemeToggle': false}, function() {
+        console.log('Value is set to ' + false);
+      });
+      setTheme(false);
+    }
+
+    modalContent.classList.remove('open');
+    modalBackground.classList.remove('open');
   });
 
   closeButton.addEventListener("click", function() {
-    closeModal();
+    modalContent.classList.remove('open');
+    modalBackground.classList.remove('open');
   });
 
   closeXButton.addEventListener("click", function() {
-    closeModal();
+    modalContent.classList.remove('open');
+    modalBackground.classList.remove('open');
   });
 }
 
-function openModal() {
-  var modalContent = document.querySelector("#modal-content");
-  var modalBackground = document.querySelector("#modal-bg");
-  initializeSaveToClipboardToggle();
-
-  modalContent.classList.add("open");
-  modalBackground.classList.add("open");
-}
-
-function closeModal() {
-  var modalContent = document.querySelector("#modal-content");
-  var modalBackground = document.querySelector("#modal-bg");
-  
-  modalContent.classList.remove("open");
-  modalBackground.classList.remove("open");
-}
-
-function initializeSaveToClipboardToggle() {
-  var toggle = document.querySelector("#toggle-checkbox");
+initializeSaveToClipboard = () => {
   chrome.storage.local.get('copyToClipboardToggle', function(result) {
-    console.log('Get storage value: ', JSON.stringify(result));
+    console.log('copyToClipboardToggle storage value: ', JSON.stringify(result));
+    if (Object.keys(result).length === 0) { 
+      chrome.storage.local.set({'copyToClipboardToggle': false}, function() {
+        console.log('copyToClipboardToggle is set to ' + false);
+      });
+    }
+  });
+}
+
+initializeSaveToClipboardToggle = () => {
+  var toggle = document.querySelector("#toggleCopyToClipboard");
+  chrome.storage.local.get('copyToClipboardToggle', function(result) {
+    console.log('toggleCopyToClipboard storage value: ', JSON.stringify(result));
     if (Object.keys(result).length === 0) {
-      if (!toggle.checked) {
-        toggle.click();
-        chrome.storage.local.set({'copyToClipboardToggle': true}, function() {
-          console.log('Value is set to ' + true);
-        });
-      }
+      chrome.storage.local.set({'copyToClipboardToggle': false}, function() {
+        console.log('toggleCopyToClipboard is set to ' + false);
+      });
     } else {
       if (result.copyToClipboardToggle) {
         if (!toggle.checked) {
+          toggle.click();
+        }
+      } else {
+        if (toggle.checked) {
           toggle.click();
         }
       }
@@ -122,36 +173,82 @@ function initializeSaveToClipboardToggle() {
   });
 }
 
-function isSaveToClipboardToggled() {
-  var toggle = document.querySelector("#toggle-checkbox");
+isSaveToClipboardToggled = () => {
+  var toggle = document.querySelector("#toggleCopyToClipboard");
   if (toggle.checked) {
-    console.log("Toggle checked");
+    console.log("toggleCopyToClipboard checked");
     return true;
   } else {
-    console.log("Toggle un-checked")
+    console.log("toggleCopyToClipboard un-checked")
     return false;
   }
 }
 
-function initializeSelectionComponent() {
-  const selected = document.querySelector(".selected");
-  const optionsContainer = document.querySelector(".options-container");
-
-  const optionsList = document.querySelectorAll(".option");
-
-  selected.addEventListener("click", () => {
-    optionsContainer.classList.toggle("active");
-  });
-
-  optionsList.forEach(o => {
-    o.addEventListener("click", () => {
-      selected.innerHTML = o.querySelector("label").innerHTML;
-      optionsContainer.classList.remove("active");
-    });
+initializeTheme = () => {
+  chrome.storage.local.get('darkThemeToggle', function(result) {
+    console.log('darkThemeToggle storage value: ', JSON.stringify(result));
+    if (Object.keys(result).length === 0) { 
+      chrome.storage.local.set({'darkThemeToggle': false}, function() {
+        console.log('darkThemeToggle is set to ' + false);
+      });
+      // setTheme(false);
+    } else {
+      setTheme(result.darkThemeToggle);
+    }
   });
 }
 
-function copyToClipboard() {
+initializeDarkThemeToggle = () => {
+  var toggle = document.querySelector("#toggleDarkTheme");
+  chrome.storage.local.get('darkThemeToggle', function(result) {
+    console.log('darkThemeToggle storage value: ', JSON.stringify(result));
+    if (Object.keys(result).length === 0) { 
+      chrome.storage.local.set({'darkThemeToggle': false}, function() {
+        console.log('darkThemeToggle is set to ' + false);
+      });
+    } else {
+      if (result.darkThemeToggle) {
+        if (!toggle.checked) {
+          toggle.click();
+        }
+      } else {
+        if (toggle.checked) {
+          toggle.click();
+        }
+      }
+    }
+  });
+}
+
+isDarkThemeToggled = () => {
+  var darkThemeToggle = document.querySelector("#toggleDarkTheme");
+  if (darkThemeToggle.checked) {
+    console.log("toggleDarkTheme checked");
+    return true;
+  } else {
+    console.log("toggleDarkTheme un-checked")
+    return false;
+  }
+}
+
+setTheme = (theme) => {
+  if (theme) {
+    setThemeTransition();
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    setThemeTransition();
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
+}
+
+setThemeTransition = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('transition');
+  }, 1000);
+}
+
+copyToClipboard = () => {
   var copyText = document.querySelector("#output");
   copyText.select();
   copyText.setSelectionRange(0, 99999)
@@ -160,25 +257,7 @@ function copyToClipboard() {
   // alert("Copied the text: " + copyText.value);
 }
 
-// function setStorageValue(key, value) {
-//   console.log('[function]: setStorageValue()')
-//   console.log('key =', key);
-//   console.log('value =', value);
-//   chrome.storage.local.set({'copyToClipboardToggle': value}, function() {
-//     console.log('Value is set to ' + value);
-//   });
-// }
-
-// function getStorageValue(key) {
-//   console.log('[function]: getStorageValue()')
-//   console.log('key = ', key);
-//   chrome.storage.local.get('copyToClipboardToggle', function(result) {
-//     console.log('Value currently is ' + JSON.stringify(result));
-//   });
-// }
-
-
-function formatStackTrace(stackTrace) {
+formatStackTrace = (stackTrace) => {
 
   if (!stackTrace || stackTrace == "") {
     return '';
@@ -189,47 +268,64 @@ function formatStackTrace(stackTrace) {
   // The list of tokens to find and prepend new lines.
   const replacementTokens = [
     {
-        original: /---\s*at/g,
-        replacement: '---\n    at'
+      format: /---\s*at/g,
+      replace: '---\n    at'
     },
     {
-        original: /\s+at /g,
-        replacement: '\n    at '
+      format: /\s+at /g,
+      replace: '\n    at '
     },
     {
-        original: /--- End of inner exception stack trace ---/g, 
-        replacement: '\n    --- End of inner exception stack trace ---'
+      format: /--- End of inner exception stack trace ---/g, 
+      replace: '\n    --- End of inner exception stack trace ---'
     },
     {
-      original: /--->/g,
-      replacement: '\n  --->'
+      format: /--->/g,
+      replace: '\n  --->'
     },
     {
-        original: /Caused by:/g,
-        replacement: '\nCaused by:'
+      format: /Caused by:/g,
+      replace: '\nCaused by:'
     },
     {
-        original: /   \.\.\. /g,
-        replacement: '\n   ... '
+      format: /   \.\.\. /g,
+      replace: '\n   ... '
     },
     {
-        // This rule should be last. Removes extra whitespace at the end of the lines.
-        original: /\s+\n/g, 
-        replacement: '\n'
+      format: /\s+\n/g, 
+      replace: '\n'
     },
   ]
 
   replacementTokens.forEach(token => {
-      result = result.replace(token.original, token.replacement);
+      result = result.replace(token.format, token.replace);
   });
 
   return result;
 }
 
-function removeNewLines(stackTrace) {
+removeNewLines = (stackTrace) =>  {
   return stackTrace.replace(/(\r\n|\r|\n)/g, '');
 }
 
+
+// function initializeSelectionComponent() {
+//   const selected = document.querySelector(".selected");
+//   const optionsContainer = document.querySelector(".options-container");
+
+//   const optionsList = document.querySelectorAll(".option");
+
+//   selected.addEventListener("click", () => {
+//     optionsContainer.classList.toggle("active");
+//   });
+
+//   optionsList.forEach(o => {
+//     o.addEventListener("click", () => {
+//       selected.innerHTML = o.querySelector("label").innerHTML;
+//       optionsContainer.classList.remove("active");
+//     });
+//   });
+// }
 
 
 
